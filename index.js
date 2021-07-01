52d6036ebe0d17d1e57fd1f8aed8ec540103ef2f
techboolo <teklufa@gmail.com>
2021-07-01 11:48:22 +0300

    filter the query with the parameter phone(enum)

--- index.js
+++ index.js
@@ -45,16 +45,32 @@ const typeDefs = gql`
        city: String!
     ): Person
   }
+
+  enum YesNo {
+    YES
+    NO
+  }
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 `
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if(!args.phone) {
+        return persons
+      }
+      const byPhone = (person) => 
+        args.phone === 'YES' 
+          ? person.phone 
+          : !person.phone
+
+      return persons.filter(byPhone)
+    },
+
     findPerson: (root, args) => 
       persons.find(p => p.name === args.name)
   },
